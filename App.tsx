e6ebc09bb079725fc08340465d0e5eae0cd081a2
Hao <hao@haos-mbp.lan>
2024-11-16 20:07:07 -0800

    RNSkia Backdrop Filter Problem
    Repro: yarn run-ios / yarn run-android

--- App.tsx
+++ App.tsx
@@ -16,7 +16,15 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
+import {
+  Canvas,
+  Circle,
+  Group,
+  BackdropFilter,
+  Blur,
+  rrect,
+  rect,
+} from '@shopify/react-native-skia';
 import {
   Colors,
   DebugInstructions,
@@ -56,43 +64,22 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 }
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const clip = rrect(rect(100, 50, 100, 100), 50, 50);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Canvas style={{flex: 1}}>
+      <Group>
+        <Circle cx={100} cy={100} r={50} color="blue" />
+        <BackdropFilter
+          filter={<Blur blur={12}/>}
+          clip={clip}>
+          <Group
+            clip={clip}>
+            <Circle cx={150} cy={100} r={50} color="red" opacity={0.7} />
+          </Group>
+        </BackdropFilter>
+      </Group>
+    </Canvas>
   );
 }
 
